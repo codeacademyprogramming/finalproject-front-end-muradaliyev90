2c507820152e6666747b3d75cd333865d19420e8
muradaliyev90 <muradaal@code.edu.az>
2020-09-12 02:22:51 +0400

    first and second pages of project

---
+++ assets/js/main.js
@@ -0,0 +1,121 @@
+$(document).ready(function () {
+    $('.owl-one').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots:true,
+        autoplay:true,
+        navContainer: '.owl-one-nav',
+        navElement: 'span',
+        navText: ['<img src="./assets/images/prevarray.svg" alt="">','<img src="./assets/images/nextarray.svg" alt="">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.owl-two').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots:true,
+        autoplay:false,
+        navContainer: '.owl-two-nav',
+        navElement: 'span',
+        navText: ['<img src="./assets/images/prevarray.svg" alt="">','<img src="./assets/images/nextarray.svg" alt="">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.owl-three').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots:true,
+        autoplay:false,
+        navContainer: '.owl-three-nav',
+        navElement: 'span',
+        navText: ['<img src="./assets/images/prevarray.svg" alt="">','<img src="./assets/images/nextarray.svg" alt="">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1170: {
+                items: 6
+            }
+        }
+    })
+});
+
+//course-page
+
+$(document).ready(function () {
+    $('.courseowl-one').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots:true,
+        autoplay:true,
+        navContainer: '.courseowl-one-nav',
+        navElement: 'span',
+        navText: ['<img src="./assets/images/prevarray.svg" alt="">','<img src="./assets/images/nextarray.svg" alt="">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1170: {
+                items: 2
+            }
+        }
+    })
+});
+
+$(document).ready(function () {
+    $('.owl-get-choice').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        dots:true,
+        autoplay:true,
+        navContainer: '.owl-get-choice-nav',
+        navElement: 'span',
+        navText: ['<img src="./assets/images/prevarray.svg" alt="">','<img src="./assets/images/nextarray.svg" alt="">'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    })
+});
